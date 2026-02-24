7699d8e03932e896e9ffe106307e4238a9d627e6
you06 <you1474600@gmail.com>
2026-02-25 01:30:52 +0900

    done: week1/day2/task1
    Signed-off-by: you06 <you1474600@gmail.com>

--- src/tiny_llm/positional_encoding.py
+++ src/tiny_llm/positional_encoding.py
@@ -9,9 +9,27 @@ class RoPE:
         base: int = 10000,
         traditional: bool = False,
     ):
-        pass
+        self.freqs = 1.0 / (base ** (2 * mx.arange(0, dims // 2) / dims))
 
     def __call__(
         self, x: mx.array, offset: list[slice] | slice | None = None
     ) -> mx.array:
-        pass
+        x_pairs = x.reshape(x.shape[:-1] + (x.shape[-1] // 2, 2))
+        if offset is None:
+            offset = 0
+        else:
+            offset = offset.start
+        l = mx.arange(0, x.shape[1]) + offset
+        freqs = mx.outer(l, self.freqs)
+        sin_freqs = mx.sin(freqs)[None, :, None, :]
+        cos_freqs = mx.cos(freqs)[None, :, None, :]
+        sin_cos_freqs = mx.array([sin_freqs, cos_freqs]).transpose(1, 2, 3, 4, 0)
+        cos_sin_freqs = mx.array([cos_freqs, sin_freqs]).transpose(1, 2, 3, 4, 0)
+        sin_cos_pairs = x_pairs * sin_cos_freqs # [x[2*i] * sin_freqs[pos, i], x[2*i+1] * cos_freqs[pos, i]]
+        cos_sin_pairs = x_pairs * cos_sin_freqs # [x[2*i] * cos_freqs[pos, i], x[2*i+1] * sin_freqs[pos, i]]
+
+        result2i = cos_sin_pairs[..., 0] - cos_sin_pairs[..., 1]
+        result2i1 = sin_cos_pairs[..., 0] + sin_cos_pairs[..., 1]
+        output = mx.stack([result2i, result2i1], axis=-1)
+        output = output.reshape(x.shape)
+        return output
